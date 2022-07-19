a8fd89de96a4403ab4972564ca80314c6f5a3f7b
Nicholas <swaggychoz@gmail.com>
2022-07-19 15:07:44 +0800

    Adjust the positions of the button and the grid

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const bigContainer = document.querySelector('.bigContainer');
 
 const button =  document.querySelector('.button');
-button.textContent = 'MAMAMIA!'
+button.textContent = 'Create a Grid!'
+
+button.addEventListener('click', createGrid);
 
 function createGrid() {
+  bigContainer.replaceChildren();
 
-  button.addEventListener('click', insertGridValue);
-
-  function insertGridValue() {
-    let column = prompt('Add values to column', '');
-    let row = prompt('Add values to row', '');
+  let column = prompt('Add values to column', '');
+  let row = prompt('Add values to row', '');
+  if (column > 100|| row > 100) {
+    alert('Please insert a value below 100');
+    return;
+  }
   
-    for (let i = 0; i < column; i++) {
-      const gridContainer = document.createElement('div'); //creates 16 containers in a row (after flex)
+  for (let i = 0; i < column; i++) {
+    const gridContainer = document.createElement('div'); //creates 16 containers in a row (after flex)
+    gridContainer.classList.add('gridContainer');
 
-      for (let k = 0; k < row; k++) {
-        const div = document.createElement('div');
-        div.classList.add('box');
-        div.addEventListener('mouseenter', () => {
-          div.classList.add('colorChange');
-        });
-        gridContainer.appendChild(div); //creates 16 divs in all of the gridContainers
-        bigContainer.appendChild(gridContainer); //stores 16 gridContainers in a big container
-      }
+    for (let k = 0; k < row; k++) {
+      const div = document.createElement('div');
+      div.classList.add('box');
+      div.addEventListener('mouseenter', () => {
+      div.classList.add('colorChange');
+      });
+      div.addEventListener('mouseleave', () => {
+      div.classList.remove('colorChange');
+      });
+      gridContainer.appendChild(div); //creates 16 divs in all of the gridContainers
+      bigContainer.appendChild(gridContainer); //stores 16 gridContainers in a big container
     }
   }
 }
-
-createGrid();
+ 
 
 
