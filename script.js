5031ab843af002969649efd449155d2802d7cebd
Nicholas <swaggychoz@gmail.com>
2022-07-19 13:51:43 +0800

    Make the button function work

--- script.js
+++ script.js
@@ -3,32 +3,26 @@ const bigContainer = document.querySelector('.bigContainer');
 const button =  document.querySelector('.button');
 button.textContent = 'MAMAMIA!'
 
-function insertColValue() {
-  let column = prompt('Add values to column', '');
-  return column;
-}
-
-function insertRowValue() {
-  let row = prompt('Add values to row', '');
-  return row;
-}
-
-button.addEventListener('click', insertColValue);
-button.addEventListener('click', insertRowValue);
-
 function createGrid() {
 
-  for (let i = 0; i < valueOne; i++) {
-    const gridContainer = document.createElement('div'); //creates 16 containers in a row (after flex)
+  button.addEventListener('click', insertGridValue);
 
-    for (let k = 0; k < valueTwo; k++) {
-      const div = document.createElement('div');
-      div.classList.add('box');
-      div.addEventListener('mouseenter', () => {
-        div.classList.add('colorChange');
-      });
-      gridContainer.appendChild(div); //creates 16 divs in all of the gridContainers
-      bigContainer.appendChild(gridContainer); //stores 16 gridContainers in a big container
+  function insertGridValue() {
+    let column = prompt('Add values to column', '');
+    let row = prompt('Add values to row', '');
+  
+    for (let i = 0; i < column; i++) {
+      const gridContainer = document.createElement('div'); //creates 16 containers in a row (after flex)
+
+      for (let k = 0; k < row; k++) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('mouseenter', () => {
+          div.classList.add('colorChange');
+        });
+        gridContainer.appendChild(div); //creates 16 divs in all of the gridContainers
+        bigContainer.appendChild(gridContainer); //stores 16 gridContainers in a big container
+      }
     }
   }
 }
